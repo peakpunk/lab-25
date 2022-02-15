1ebae91364fc19dfe7199191142ab5a0a3b6dd36
peakpunk <95071107+peakpunk@users.noreply.github.com>
2022-02-15 18:12:31 +0700

    complete lab25

--- lab25.cpp
+++ lab25.cpp
@@ -28,7 +28,43 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(c.real*imag)+(real*c.imag));
+}
+ComplexNumber operator*(double x,const ComplexNumber &c){
+	return ComplexNumber(x*c.real,x*c.imag);
+}
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber(((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2)),((c.real*imag)-(real*c.imag))/(pow(c.real,2)+pow(c.imag,2)));
+}
+ComplexNumber operator/(double x,const ComplexNumber &c){
+	return ComplexNumber((x*c.real)/(pow(c.real,2)+pow(c.imag,2)),((x*-1)*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if((real==c.real)&&(imag==c.imag)) return true;
+	else return false;
+}
+bool operator==(double s,const ComplexNumber &c){
+	if(s==c.real) return true;
+	else return false;
+}
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+double ComplexNumber::angle(){
+	return atan2(imag,real)*180/M_PI;
+}
+
+ostream& operator<<(ostream &os,const ComplexNumber &c){
+	if(c.real==0&&c.imag==0) return os << c.real;
+	else if(c.real==0&&c.imag>0) return os << c.imag << "i";
+	else if(c.real!=0&&c.imag>0) return os << c.real << "+" << c.imag << "i";
+	else if(c.real!=0&&c.imag==0) return os << c.real;
+	else if(c.real>0&&c.imag<0) return os << c.real << c.imag << "i";
+	else if(c.real<0&&c.imag>0) return os << c.real << "+" << c.imag << "i";
+	else if(c.real<0&&c.imag<0) return os <<c.real<<c.imag<<"i";
+	else return os << c.imag << "i";
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
